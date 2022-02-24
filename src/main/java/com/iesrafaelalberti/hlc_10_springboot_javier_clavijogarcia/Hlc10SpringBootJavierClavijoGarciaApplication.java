d3df98b1d1e25895923a531fcee1a12b880a13e6
Javier Clavijo <javierclavijog@gmail.com>
2022-02-24 16:31:24 +0100

    Initial commit

---
+++ src/main/java/com/iesrafaelalberti/hlc_10_springboot_javier_clavijogarcia/Hlc10SpringBootJavierClavijoGarciaApplication.java
@@ -0,0 +1,13 @@
+package com.iesrafaelalberti.hlc_10_springboot_javier_clavijogarcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hlc10SpringBootJavierClavijoGarciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hlc10SpringBootJavierClavijoGarciaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iesrafaelalberti/hlc_10_springboot_javier_clavijogarcia/Hlc10SpringBootJavierClavijoGarciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesrafaelalberti.hlc_10_springboot_javier_clavijogarcia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hlc10SpringBootJavierClavijoGarciaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
